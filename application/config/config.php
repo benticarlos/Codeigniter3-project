0c494a51a4daf98b6bd8d3d87c9938f6db0e47c8
Carlos Benti <benticarlos@gmail.com>
2017-02-02 15:53:07 -0400

    Corrected: config in session library (files)

--- application/config/config.php
+++ application/config/config.php
@@ -380,16 +380,16 @@ $config['encryption_key'] = '4a5a5dca22728fb0a84364eeb405b601';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-// $config['sess_driver'] = 'files';
-$config['sess_driver'] = 'database';
+$config['sess_driver'] = 'files';
+// $config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_save_path'] = 'ci_session';
+// $config['sess_save_path'] = 'ci_session';
 $config['sess_expiration'] = 7200;
 // $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
-// $config['sess_save_path'] = sys_get_temp_dir();
+$config['sess_save_path'] = sys_get_temp_dir();
 
 /*
 |--------------------------------------------------------------------------
