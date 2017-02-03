99ca187a9aead10ad54f27897331d7699dab2692
Carlos Benti <benticarlos@gmail.com>
2017-02-03 09:13:10 -0400

    Deleted ->load->library('session') from Auth controller

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,7 +6,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		public function __construct()
 		{
 			parent::__construct();
-			$this->load->library('session');
 			$this->load->model("Auth_model");
 		}
 
